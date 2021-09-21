eee27d6d69671015fb5b0ddd04eede433151fda8
franz0406 <80723523+franz0406@users.noreply.github.com>
2021-09-21 21:54:20 +0900

    Delete css directory

--- css/js/nav_btn.js
+++
@@ -1,9 +0,0 @@
-const navBtn = document.querySelector(".nav-btn");
-const navBar = document.querySelector(".header > nav");
-const navClose = document.querySelector(".nav-close");
-navBtn.addEventListener('click', function(){
-    navBar.classList.toggle("active");
-})
-navClose.addEventListener('click', function(){
-    navBar.classList.toggle("active");
-})
